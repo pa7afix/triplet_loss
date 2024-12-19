b652bec7414c8eeb539e72207d72883c9cbefba4
Patrick Schäfer <patrick.schaefer@uni-due.de>
2024-12-19 15:18:15 +0100

    initial setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import tensorflow as tf
+from pathlib import Path
+from keras import applications
+from keras import layers
+from keras import losses
+from keras import ops
+from keras import optimizers
+from keras import metrics
+from keras import Model
+from keras.applications import resnet
+
